853b8465c46af3bb468baf94fe5ebf7971334c90
Rishaw kumar <72737691+Rishaw-Developer@users.noreply.github.com>
2021-01-22 01:41:55 +0530

    Chaged organised the variables, methods, etc names
    I have changed my unreadable variables into readable and understandable variables.

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ print("Hy user , Welcome to the R-Maill where you send your mails to anyones you
 
 class Email:
         
-    def send_mail(self, sender_to_where,message):
+    def send_mail(self, send_to_where, message):
         '''This method will send the message'''
-        with open(f'{sender_to_where}.txt', "a") as send:
-            send.write(f"At {datetime.datetime.now()} : {message}, \n")
-            send.close()
+
+        with open(f'{send_to_where}.txt', "a") as sending_message:
+            sending_message.write(f"At {datetime.datetime.now()} : {message}, \n")
+            sending_message.close()
 
     def create_account(self):
         '''This method will create an account if no account exists'''
+
         account_email = input("Enter your E-mail Address to create a new account: ")
         account_password = input("Enter your E-Mail Password to create an account: ")
-        with open('login_signup.txt', 'a') as create:
-            create.write(f'{account_email}\n')
-            create.write(f'{account_password}\n')
-            create.close()
+        with open('login_signup.txt', 'a') as creating_account:
+            creating_account.write(f'{account_email}\n')
+            creating_account.write(f'{account_password}\n')
+            creating_account.close()
 
 
 
     def login_account(self):
+        '''This method will be used to login'''
+
         account_email = input("Enter your E-Mail Address to login : ")
 
         if account_email in open('login_signup.txt').read():
@@ -47,29 +51,35 @@ class Email:
 
 
     def read_mails(self):
-        what_to_read = input("Whose mails do you want to read : ")
-        file_path = r'/media/rishaw/Local Disk/R-Mail Project/' + what_to_read + '.txt'
+        '''This method will be used to read the mails'''
+
+        whose_mail = input("Whose mails do you want to read : ")
+        file_path = r'/media/rishaw/Local Disk/R-Mail Project/' + whose_mail + '.txt'
         if os.path.exists(file_path):
-            with open(f'{what_to_read}.txt') as readbro:
-                print(readbro.read())
+            with open(f'{whose_mail}.txt') as reading_mail:
+                print(reading_mail.read())
 
 
 
     def delete_mail(self):
-        game = input("Whose mails should be deleted: ")
-        if os.path.exists(f"{game}.txt"):
-            os.remove(f"{game}.txt")
+        '''This method will be used to delete the all the mails of a specific email'''
+
+        removing_mail = input("Whose mails should be deleted: ")
+        if os.path.exists(f"{removing_mail}.txt"):
+            os.remove(f"{removing_mail}.txt")
         else:
             print("The file does not exist")
 
 
 
     def delete_account(self):
-        decision_account = input("Are you sure[Y/N] : ")
-        if decision_account == 'y' or 'Y':
-            with open('login_signup.txt', 'wt') as delta_account:
-                delta_account.write('')
-                delta_account.close()
+        '''This method will delete the account and password of the user'''
+
+        deleting_decision = input("Are you sure[Y/N] : ")
+        if deleting_decision == 'y' or 'Y':
+            with open('login_signup.txt', 'wt') as removing_account:
+                removing_account.write('')
+                removing_account.close()
 
             print("Your account is deleted successfully")
 
@@ -80,21 +90,22 @@ Account = Email()
 
 
 def main_senders():
-    with open('login_signup.txt') as get_name:
-        get = get_name.readline().split('.')
-        print(f"Welcome {get[0]}")
-        get_name.close()
+    '''This function controls the all sending, reading, and deleting of a account or mail'''
+    with open('login_signup.txt') as user_name:
+        get_name = user_name.readline().split('.')
+        print(f"Welcome {get_name[0]}")
+        user_name.close()
 
-    dence = False
-    while dence is not True:
+    ender = False
+    while ender is not True:
         print("\nEnter 's' to send the mail, 'r' to read the mails, 'c' to delete the account, 'd' to delete the mail contact, and 'q' to quite")
 
         select = input("Select : ")
         take = select.lower()
         if take == 's':
-            where_to_get = input("Enter the mail : ")
+            where_to_send = input("Enter the mail : ")
             message = input("Enter your message here : ")
-            Account.send_mail(where_to_get, message)
+            Account.send_mail(where_to_send, message)
 
 
 
@@ -111,12 +122,12 @@ def main_senders():
         elif take == 'c':
             Account.delete_account()
             time.sleep(0.3)
-            thakur = input("Type 'n' for create a new account and 'z' to exit : ")
+            last_decision = input("Type 'n' for create a new account and 'z' to exit : ")
 
-            if thakur == 'n':
+            if last_decision == 'n':
                 main()
 
-            elif thakur == 'z':
+            elif last_decision == 'z':
                 sys.exit('You have quit the code')
 
             else:
@@ -124,7 +135,7 @@ def main_senders():
 
 
         elif take == 'q' or 'quite':
-            dence = True
+            ender = True
 
 
 
@@ -132,6 +143,7 @@ def main_senders():
             print('LOL, INVALID')
 
 def main():
+    '''All the things of this program is starts from here'''
 
 
     if os.stat("login_signup.txt").st_size == 0:
